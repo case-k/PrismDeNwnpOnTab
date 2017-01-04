37d3226375adefe1a3ef8557640cd8df734300f2
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2017-01-04 10:47:31 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeNwnpOnTab/PrismDeNwnpOnTab/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeNwnpOnTab.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
